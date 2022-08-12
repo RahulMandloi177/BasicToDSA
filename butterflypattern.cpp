d8af3e4800e6dd27f80f2b22475fb0b2251c1a27
RahulMandloi177 <106391125+RahulMandloi177@users.noreply.github.com>
2022-08-12 10:37:59 +0530

    Add files via upload

---
+++ butterflypattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   int i,j,row;
+    cout<<"Enter the number of rows ";
+    cin>>row;
+
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        int space =2*row-2*i;
+        for(j=1;j<=space;j++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<< endl;
+    }
+
+    for(i=row;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        int space =2*row-2*i;
+        for(j=1;j<=space;j++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<< endl;
+    }
+}
