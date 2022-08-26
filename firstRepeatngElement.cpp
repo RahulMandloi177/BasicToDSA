7c322afe364cc6f9ae11a1d22e8bc772ac949492
RahulMandloi177 <106391125+RahulMandloi177@users.noreply.github.com>
2022-08-26 18:20:18 +0530

    Add files via upload

---
+++ firstRepeatngElement.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{   
+    int i,j,size;
+    cout<<"Enter the size of array";
+    cin>>size;
+
+    int arr[size];
+    
+    cout<<"Enter the elemets of array";
+    for(i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+   const int N=1e6+2;
+   int idx[N];
+   for(i=0;i<N;i++)
+   {
+       idx[i]=-1;
+   }
+    int minidx=INT_MAX;
+    for(i=0;i<size;i++)
+    {
+        if(idx[arr[i]]!=-1)
+        {
+            minidx=min(minidx,idx[arr[i]]);
+        }
+        else
+        idx[arr[i]]=i;
+    }
+    if (minidx==INT_MAX)
+    {
+        cout<<"-1"<<endl;
+    }
+    else
+    {
+       cout<<minidx+1<<endl;
+    }
+}   
