db56b3ccb7e3ca662925ce21dcfa9f99357132b3
RahulMandloi177 <106391125+RahulMandloi177@users.noreply.github.com>
2022-09-01 20:33:57 +0530

    Add files via upload

---
+++ maxAndMinDifferenceOfArray.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array ";
+    cin>>n;
+
+    vector<int> a(n);
+
+    for(auto &i :a)
+    {
+        cin>>i;
+    }
+
+    sort(a.begin(),a.end());
+
+    long long min=0,max=0;
+
+    for(int i=0;i<n/2;i++)
+    {
+        max+=(a[i+n/2]-a[i]);
+        min+=(a[2*i+1]-a[2*i]);
+    }
+
+    cout<<min<<" "<<max;
+}
