342354641b2f6d75bae76a391d901f4b7be923c3
RahulMandloi177 <106391125+RahulMandloi177@users.noreply.github.com>
2022-10-08 13:16:07 +0530

    Add files via upload

---
+++ pascaltriangle.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int factorial=1;
+    for(int i=2;i<=n;i++)
+    {
+        factorial=factorial*i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int row;
+    cout<<"Enter the rows ";
+    cin>>row;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+        }
+        cout<<endl;
+    }
+
+    
+    
+}
