240906bab5f9ed1b4ee4d865e0b613ba8a0e1d0c
RahulMandloi177 <106391125+RahulMandloi177@users.noreply.github.com>
2022-10-08 13:15:28 +0530

    Add files via upload

---
+++ minSquare.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+2,MOD=1e9+7;
+
+int dp[N];
+
+int minSquare(int n)
+{
+    if(n==0||n==1||n==2||n==3)
+    {
+        return n;
+    }
+    
+    if(dp[n]!=MOD)
+    {
+        return dp[n];
+    }
+
+    for (int i =1; i*i <=n; i++)
+    {
+        dp[n]=min(dp[n],1+minSquare(n-i*i));
+    }
+    
+    return dp[n];
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<N;i++)
+    {
+        dp[i]=MOD;
+    }
+
+    cout<<minSquare(n)<<endl;
+    return 0;
+}
